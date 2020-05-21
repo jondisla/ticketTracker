af6ac371c8820f43688f8fbf3eb13d8d21999fd3
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-05-20 21:13:25 -0400

    models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Routes for tickets
+Route::resource('Tickets', 'TicketsController');
