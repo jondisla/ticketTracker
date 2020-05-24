a642f75b3f223bd58c78dd3af23141735e88051c
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-05-24 08:31:08 -0400

    index build sun. 8:31

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -20,6 +20,27 @@
             </ul>
         </div>
       </div>
+
+
+
+      <div class="testcard">
+        {{-- <img class="card-img-top" src="holder.js/100px180/" alt=""> --}}
+          <div class="small timestamp">Customer Name  |  DATE</div>
+          <h4 class="card-title">My Printer is not working</h4>
+          <div class="divider"></div>
+          <ul class="details">
+              <li>Ticket ID:</li>
+              <li>Department: </li>
+          </ul>
+          <div class="buttons">
+              <ul>
+                  <li>D</li>
+                  <li>D</li>
+                  <li>D</li>
+              </ul>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
 
