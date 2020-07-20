1051a0e823e3db40320d92d40f6f56e65e9be0ca
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-07-19 21:09:31 -0400

    about page start

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>This is the about page.</h3>
+<div class="wrapper">
+    <h3>Ticket Tracker</h3>
+    <p>This project has been brought to you by Jonathan Disla (Web Developer)</p>
+</div>
 
 @endsection
