a6fc79c02833d5cc16711a606ce68304575ce497
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-07-25 21:48:38 -0400

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('departments')->insert(array (
+        DB::table('departments')->create(array (
             0 => 
               array (
                      'department' => 'IT'
