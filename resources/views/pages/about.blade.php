a9ba309d81e5912067f5975c6d4e84d267290fea
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-07-25 22:08:15 -0400

    img

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
 <div class="text-center">
     <h3>Ticket Tracker</h3>
     <h5>This project has been brought to you by Jonathan Disla (Web Developer)</h5>
-    <img style ="border-radius:50%;" src="img/me.jpeg" alt="">
+    <img style ="border-radius:50%;width:80vw;" src="img/me.jpeg" alt="">
     <br>
     <br>
     <h5><a href="https://johndisla.com">Goto Website</a></h5>
